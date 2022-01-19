448947195e15d0fc70f0a4032f430fa33f128ea2
Andrés González Paul Rivera <andres.g.paul@gmail.com>
2022-01-19 22:01:34 +0100

    added test osc messages from python to processing

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, redirect, url_for, request, session
+from flask import Flask, redirect, url_for, request, session, jsonify
+from osc_test import getParams
 import spotipy
 import os
 import time
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+
+import argparse
+import random
+from pythonosc import udp_client
+from pythonosc import osc_server
+from pythonosc import dispatcher
 
 app = Flask(__name__)
 
@@ -10,6 +17,7 @@ app.config["DEBUG"] = True
 app.secret_key = "IU86bytdtb72boLAui"
 app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
 
+client = udp_client.SimpleUDPClient("127.0.0.1", 1234)
 
 @app.route("/")
 def login():
@@ -17,6 +25,14 @@ def login():
     sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
+    # global sp
+    # cid = "49e612edb8144e78befdfceaf0612429"
+    # secret = "73fab72888874b4483f74f64ffad137c"
+    # client_credentials_manager = SpotifyClientCredentials(
+    #     client_id=cid, client_secret=secret)
+    # sp = spotipy.Spotify(
+    #     client_credentials_manager=client_credentials_manager, requests_timeout=200)
+    # return redirect("/getParameters")
 
 def create_spotify_oauth():
     return SpotifyOAuth(
@@ -40,7 +56,9 @@ def getParameters():
     session.modified = True
     if not authorized:
         return redirect('/')
+
     sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    
     # track = sp.current_user_top_tracks(limit=1, offset=0, time_range="medium_term")["items"]
     # track_name = track[0]["name"]
     # track_URI = track[0]["uri"]
@@ -48,8 +66,11 @@ def getParameters():
     # acousticness = track_features["acousticness"]
     # valence = track_features["valence"]
     # params= {"TrackName":track_name, "Acousticness":acousticness, "Valence":valence}
+
     user = sp.current_user()
-    return user #params
+    global w_msg
+    w_msg = "Welcome " + user["display_name"]
+    return w_msg #user #params
 
 def get_token():
     token_valid = False
@@ -68,3 +89,26 @@ def get_token():
 
     token_valid = True
     return token_info, token_valid
+
+def sendvalues(unused_addr):
+
+    msg = 987 #getParameters()
+    
+    client.send_message("/mousepressed", "{}".format(jsonify(msg)))
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip", default="127.0.0.1",
+      help="The ip of the OSC server")
+  parser.add_argument("--port", type=int, default=1234,
+      help="The port the OSC server is listening on")
+  args = parser.parse_args()
+
+  dispatcher = dispatcher.Dispatcher()
+
+  dispatcher.map("/miklo",sendvalues)
+  
+  server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+
+  print("Serving on {}".format(server.server_address))
+  server.serve_forever()
---
+++ send_osc_test.py
@@ -0,0 +1,34 @@
+import argparse
+import random
+import time
+
+from pythonosc import udp_client
+from pythonosc import osc_server
+from pythonosc import dispatcher
+
+client = udp_client.SimpleUDPClient("127.0.0.1", 1234)
+
+def sendvalues(unused_addr):
+    msg = getParams()
+  
+    client.send_message("/mousepressed", "{}".format(msg))
+
+def getParams():
+    return 123
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+        help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=5005,
+        help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    dispatcher = dispatcher.Dispatcher()
+
+    dispatcher.map("/miklo",sendvalues)
+
+    server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
