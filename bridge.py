8ba8a62ed80678062b10ec19f0e5d0bbff8f85d4
FrancescoZumo <francescozumo@gmail.com>
2022-01-21 17:58:26 +0100

    update bridge.py

--- bridge.py
+++ bridge.py
@@ -1,3 +1,7 @@
 import os
 
-print(os.popen('python style_transfer_demo.py 1 1 dummy_cropped.jpg').read())
+acousticness = 1
+valence = 1
+content_image = "dummy_cropped.jpg"
+
+print(os.popen('python style_transfer_demo.py ' + str(acousticness) + ' ' + str(valence) + ' ' + content_image).read())
