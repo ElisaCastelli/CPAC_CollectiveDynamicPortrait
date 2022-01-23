4d18e7c0a7504401a2d75d356ab5d3c90d3c94c6
FrancescoZumo <francescozumo@gmail.com>
2022-01-23 09:53:17 +0100

    finished take_photo_demo.py

--- take_photo_demo.py
+++ take_photo_demo.py
@@ -10,6 +10,13 @@ import time
 import os
 import sys
 
+
+# IMPORTANT
+# load cv2.CascadeClassifier
+# I needed to specify the full path to the xml file, modify according to your pc
+face_cascade = cv2.CascadeClassifier('C:\\Users\\franc\\anaconda3\\envs\\tf-gpu\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_alt2.xml')
+
+
 # path where images are stored
 image_folder = 'style_pics/'
 file_id = sys.argv[1]
@@ -26,29 +33,80 @@ def take_photo(filename='photo.jpg', quality=0.8):
   cv2.imwrite(filename, img)
   return filename
 
+# sometimes no face is detected, so we make the program try again, 
+face_found = False
+max_attempts = 5
 
-# In[8]:
+while face_found == False:
+  # taking photo
 
-try:
-  filename = take_photo(image_folder + file_id + '.jpg')
-  print('Saved to {}'.format(filename))
-  # Show the image which was just taken.
-  display(Image(filename))
-  #display(Image(filename2))
+  try:
+    filename = take_photo(image_folder + file_id + '.jpg')
+    print('Saved to {}'.format(filename))
+    # Show the image which was just taken.
+    # display(Image(filename))
+    # display(Image(filename2))
 
-  print("exit status (checking if everything went fine):")
-  if os.path.exists(filename):
-      print(0)
+    print("exit status (checking if everything went fine):")
+    if os.path.exists(filename):
+        print(0)
+    else:
+        print(1)
+        sys.exit()
+  
+  except Exception as err:
+    # Errors will be thrown if the user does not have a webcam or if they do not
+    # grant the page permission to access it.
+    print(str(err))
+    print(2)
+    sys.exit()
+
+
+  # face detect
+    
+  # Read the input image
+  img = cv2.imread(filename)
+    
+  # Convert into grayscale
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+  # Detect faces
+  faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+  for (x, y, w, h) in faces:
+      #cv2.rectangle(img, (x, y), (x+w, y+h), 
+       #             (0, 0, 255), 2)
+
+      # increase area around face before cropping
+      x_padding = int(w*0.1)
+      y_padding = int(h*0.1)
+      max_height = img.shape[0]
+      max_width = img.shape[1]
+      # if increasing is impossible while maintaining height == width, don't increase
+      if y - y_padding < 0 or y + h + y_padding > max_height or x - x_padding < 0 or x + w + x_padding > max_width:
+        x_padding = 0
+        y_padding = 0
+
+      faces = img[y - y_padding:y + h + y_padding, x - x_padding:x + w + x_padding]
+      #cv2.imshow("face",faces)
+      cv2.imwrite(image_folder + file_id + '_face.jpg', faces)
+
+      # update flag
+      face_found = True
+
+      # just take the first face
+      break
+  
+  if face_found:
+    print(0)
   else:
-      print(1)
- 
-except Exception as err:
-  # Errors will be thrown if the user does not have a webcam or if they do not
-  # grant the page permission to access it.
-  print(str(err))
-  print(2)
+    --max_attempts
 
+  if max_attempts <= 0:
+    break
 
+if not face_found:
+  print(3)
 
-
-
+#cv2.imshow('img', img)
+#cv2.waitKey()
